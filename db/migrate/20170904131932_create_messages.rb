012611029c62baf7f5d2ec7538a8c881f8f499c3
Alexander Valko <krolaka9388@gmail.com>
2017-09-05 11:48:09 +0300

    added migration

--- db/migrate/20170904131932_create_messages.rb
+++ db/migrate/20170904131932_create_messages.rb
@@ -3,7 +3,6 @@ class CreateMessages < ActiveRecord::Migration[5.1]
     create_table :messages do |t|
       t.integer :tx_user_id
       t.integer :rx_user_id
-      t.string :subject
       t.string :message
       t.boolean :read, default: false
       t.timestamps
---
+++ db/migrate/20170905083038_add_blocked_column_to_users.rb
@@ -0,0 +1,5 @@
+class AddBlockedColumnToUsers < ActiveRecord::Migration[5.1]
+  def change
+    add_column :users, :blocked, :boolean, default: false
+  end
+end
