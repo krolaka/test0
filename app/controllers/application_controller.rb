e03a49005de9281cf432b897eaa8c8ea31b165b8
Alexander Valko <krolaka9388@gmail.com>
2017-09-08 14:19:21 +0300

    Added new controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,23 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
+
+  rescue_from CanCan::AccessDenied do |exception|
+    flash[:alert] = exception.message
+    redirect_to root_url
+  end
+
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    flash[:alert] = exception.message
+    redirect_to root_url
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:edit, keys: [:username, :email, :password, :current_password, :admin, :about])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :about])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email, :password, :current_password, :admin, :about])
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,56 @@
 class UsersController < ApplicationController
 
+  before_action :find_user, only: [:show, :edit, :destroy, :update]
+
   def index
-    @users = User.all
+  end
+
+  def list
+    @users = User.all.where.not(id: current_user.id)
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new
+    @user.assign_attributes(user_params)
+    if @user.valid? && @user.save
+      redirect_to users_path, notice: 'Profile was successfully created'
+    else
+      redirect_to new_user_path, alert: 'Something went wrong!'
+    end
+  end
+
+  def destroy
+    @user.destroy
+    redirect_to users_path, notice: "Delete success."
+  end
+
+  def update
+    @user.assign_attributes(user_params)
+    if @user.valid? && @user.save
+      redirect_to users_path, notice: 'Profile was successfully edited'
+    else
+      redirect_to edit_user(@user.id), alert: 'Something went wrong!'
+    end
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :email, :about, :admin, :blocked, :password, :password_confirmation)
   end
 
 end
