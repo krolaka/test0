61da815c3ffe02b12b5a18d3df2cb7d3f36444a2
Alexander Valko <krolaka9388@gmail.com>
2017-09-09 23:59:43 +0300

    added seed.rb for generate demo data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,60 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Which email do you want use for loggin into site and adminstrate?"
+email     = STDIN.gets.strip
+password = '111111'
+
+admin_user = User.create(:username => Faker::Internet.user_name,
+                         :email => email, :password => password,
+                         :password_confirmation => password,
+                         :admin => true
+                        )
+
+if admin_user.valid?
+  puts "Perfect! Your account was created with '111111' password."
+else
+  puts "Sorry but something went worng!"
+  puts ""
+  admin_user.errors.full_messages.each { |m| puts "   - #{m}" }
+end
+
+
+# Creates other users
+blocked = false
+i = 0
+for i in 0..5
+  email = Faker::Internet.safe_email
+  blocked = true if i == 4
+  user = User.create(:username => Faker::Internet.user_name,
+                     :email => email,
+                     :password => password,
+                     :password_confirmation => password,
+                     :blocked => blocked
+                    )
+  puts "User #{email} created with password '111111'" if user.valid?
+end
+
+puts "Generating disscussion."
+# Creates messages
+i = 0
+r = 0
+for i in 0..10
+  first_random_user = User.order('RANDOM()').limit(1).first
+  for i in 0..15
+    users = User.order('RANDOM()').limit(5)
+    if i == 0
+      Message.create(rxuser_id: first_random_user.id,
+                     txuser_id: users.last.id,
+                     message: Faker::Lorem.sentence
+                    )
+    end
+    users.each do |user|
+      last_user_id = Message.last.rxuser_id
+      Message.create(txuser_id: last_user_id,
+                     rxuser_id: user.id,
+                     message: Faker::Lorem.sentence
+                    )
+    end
+  end
+end
+puts 'Done'
